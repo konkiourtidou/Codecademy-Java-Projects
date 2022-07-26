299533c58b073563229966d57f8daddcae5acf85
konkiourtidou <dinakiourtidou1@gmail.com>
2022-07-27 00:08:04 +0300

    Add files via upload

---
+++ Lang/SinoTibetan.java
@@ -0,0 +1,8 @@
+class SinoTibetan extends Language {
+    SinoTibetan(String name, int numSpeakers){
+        super(name, numSpeakers, "Asia", "subject-object-verb");
+        if (this.name.contains("Chinese")){
+            this.wordOrder = "subject-verb-object";
+        }
+    }
+}
